854817eaef50d49a1431074378324e18df5db9a8
jesicadaiana <ajesicadaiana@gmail.com>
2024-03-07 08:38:12 -0300

    Boton

---
+++ script.js
@@ -0,0 +1,7 @@
+let boton = document.getElementById ('cambiarColor');
+const color = document.querySelector('.color');
+
+function cambioDeColor () {
+         console.info ('Funciona el boton!!!')  
+}
+
